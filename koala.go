41b99ea59bf7caafcd45b6dd22bc3440f8236327
Angel ILIEV <a.v.iliev13@gmail.com>
2022-02-16 10:16:32 +0200

    Fix formatting

--- koala.go
+++ koala.go
@@ -33,7 +33,6 @@ func walkDir(dir string) {
 }
 
 func main() {
-
     p := tea.NewProgram(InitialModel())
     if err := p.Start(); err != nil {
         fmt.Printf("Alas, there's been an error: %v", err)
