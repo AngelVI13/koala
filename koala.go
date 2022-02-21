2ca9e5903c55f9a3b3ff86792a73e023ffd910b4
Angel VI <angel@gmail.com>
2022-02-21 08:08:59 +0200

    Replaced colly with goqueryselector

--- koala.go
+++ koala.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+    "net/http"
+    "io"
     "log"
-    "github.com/gocolly/colly"
+    "github.com/PuerkitoBio/goquery"
 )
 
+func ExampleScrape() {
+  // This request gives back a json file with all holdings data
+  // curl 'https://www.ishares.com/uk/individual/en/products/280510/ishares-sp-500-information-technology-sector-ucits-etf/1506575576011.ajax?tab=all&fileType=json' -J -O -L
+
+  // Request the HTML page.
+  res, err := http.Get("https://www.ishares.com/uk/individual/en/products/280510/ishares-sp-500-information-technology-sector-ucits-etf?switchLocale=y&siteEntryPassthrough=true")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+  }
+  body, err := io.ReadAll(res.Body)
+  if err != nil {
+      log.Fatal(err)
+  }
+  log.Println(string(body))
+
+  // Load the HTML document
+  doc, err := goquery.NewDocumentFromReader(res.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Find the review items
+  doc.Find("#allHoldingsTable tbody tr").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+        log.Println(s)
+		title := s.Find("td").Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
+}
+
 func main() {
-    c := colly.NewCollector()
-
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL)
-    })
-
-    c.OnError(func(_ *colly.Response, err error) {
-        log.Println("Something went wrong:", err)
-    })
-
-    c.OnResponse(func(r *colly.Response) {
-        fmt.Println("Visited", r.Request.URL)
-    })
-
-    c.OnHTML("span.header-nav-data:nth-child(2)", func(e *colly.HTMLElement) {
-        log.Println("Price:", e.Text)
-    })
-
-    c.OnScraped(func(r *colly.Response) {
-        fmt.Println("Finished", r.Request.URL)
-    })
-
-    c.Visit("https://www.ishares.com/uk/individual/en/products/280510/ishares-sp-500-information-technology-sector-ucits-etf?switchLocale=y&siteEntryPassthrough=true")
+  ExampleScrape()
 }
